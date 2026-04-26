00bf298b762c59d6ada5d17e82ae980a2ebf9c26
HYH <huyuhao612@outlook.com>
2026-04-26 21:29:48 +0800

    云PgSQL存向量

--- src/main/java/space/huyuhao/myagent/MyAgentApplication.java
+++ src/main/java/space/huyuhao/myagent/MyAgentApplication.java
@@ -1,9 +1,11 @@
 package space.huyuhao.myagent;
 
+import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "space.huyuhao.myagent")
+
+@SpringBootApplication(scanBasePackages = "space.huyuhao.myagent",exclude = PgVectorStoreAutoConfiguration.class)
 public class MyAgentApplication {
 
     public static void main(String[] args) {
--- src/main/java/space/huyuhao/myagent/demo/invoke/LangChainAiInvoke.java
+++ src/main/java/space/huyuhao/myagent/demo/invoke/LangChainAiInvoke.java
@@ -1,17 +1,17 @@
-package space.huyuhao.myagent.demo.invoke;
-
-import dev.langchain4j.community.model.dashscope.QwenChatModel;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-
-public class LangChainAiInvoke {
-
-    public static void main(String[] args) {
-        String apiKey = System.getenv("ALI_API_KEY");
-        ChatLanguageModel qwenModel = QwenChatModel.builder()
-                .apiKey(apiKey)
-                .modelName("qwen-max")
-                .build();
-        String answer = qwenModel.chat("我用的是晨光的笔,你们都是啥笔.");
-        System.out.println(answer);
-    }
-}
+//package space.huyuhao.myagent.demo.invoke;
+//
+//import dev.langchain4j.community.model.dashscope.QwenChatModel;
+//import dev.langchain4j.model.chat.ChatLanguageModel;
+//
+//public class LangChainAiInvoke {
+//
+//    public static void main(String[] args) {
+//        String apiKey = System.getenv("ALI_API_KEY");
+//        ChatLanguageModel qwenModel = QwenChatModel.builder()
+//                .apiKey(apiKey)
+//                .modelName("qwen-max")
+//                .build();
+//        String answer = qwenModel.chat("我用的是晨光的笔,你们都是啥笔.");
+//        System.out.println(answer);
+//    }
+//}
