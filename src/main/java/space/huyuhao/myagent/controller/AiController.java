e3e0c1f3a2e8309417c814ca65863e55fea28dcf
HYH <huyuhao612@outlook.com>
2026-05-02 15:42:27 +0800

    2026-05-02 实现关于智能体接口的调用

--- src/main/java/space/huyuhao/myagent/controller/AiController.java
+++ src/main/java/space/huyuhao/myagent/controller/AiController.java
@@ -3,6 +3,7 @@ package space.huyuhao.myagent.controller;
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import reactor.core.publisher.Flux;
+import space.huyuhao.myagent.agent.MyManus;
 import space.huyuhao.myagent.app.MyApp;
 
 import java.io.IOException;
@@ -26,6 +28,9 @@ public class AiController {
 
     @Resource
     private ChatModel dashscopeChatModel;
+
+    @Resource
+    private ToolCallbackProvider toolCallbackProvider;
 
     @GetMapping("/my_app/chat/sync")
     public String doChatWithMyAppSync(String message, String chatId) {
@@ -87,6 +92,17 @@ public class AiController {
         return emitter;
     }
 
+    /**
+     * 流式调用 Manus 超级智能体
+     *
+     * @param message
+     * @return
+     */
+    @GetMapping("/manus/chat")
+    public SseEmitter doChatWithManus(String message) {
+        MyManus myManus = new MyManus(allTools, toolCallbackProvider, dashscopeChatModel);
+        return myManus.runStream(message);
+    }
 
 
 }
